d98749f00b684c7e658361dd88b30004664a6867
shun-ko <arai.shuntaro.5@gmail.com>
2021-01-28 05:04:19 +0900

    Applied to Rubocop

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
-    factory :user do
-      name {"hoge"}
-      email {"hoge@example.com"}
-    end
+  factory :user do
+    name { 'hoge' }
+    email { 'hoge@example.com' }
+  end
 end
-
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe '/users', type: :request do
-  
   describe 'GET /index' do
     before do
-      user = FactoryBot.create(:user)   
+      FactoryBot.create :user
       get users_path
     end
 
-    it "リクエストが成功すること" do
-      expect(response.status).to eq 200    
+    it 'リクエストが成功すること' do
+      expect(response.status).to eq 200
     end
 
-    it "ユーザー名とメールアドレスが表示されていること" do
-      expect(response.body).to include "hoge"
-      expect(response.body).to include "hoge@example.com"
+    it 'ユーザー名とメールアドレスが表示されていること' do
+      expect(response.body).to include 'hoge'
+      expect(response.body).to include 'hoge@example.com'
     end
   end
-  
+
   describe 'GET #show' do
     before do
       user = FactoryBot.create :user
@@ -31,15 +30,15 @@ RSpec.describe '/users', type: :request do
 
       it 'ユーザー名とメールアドレスが表示されていること' do
         expect(response.body).to include 'hoge'
-        expect(response.body).to include "hoge@example.com"
+        expect(response.body).to include 'hoge@example.com'
       end
     end
 
     context 'ユーザーが存在しない場合' do
       # subject { -> { get user_path 1 } }
       # it { is_expected.to raise_error ActiveRecord::RecordNotFound }
-      it "エラーが表示されること"do
-        expect{ get user_path 1 }.to raise_error ActiveRecord::RecordNotFound
+      it 'エラーが表示されること' do
+        expect { get user_path 1 }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
@@ -49,7 +48,7 @@ RSpec.describe '/users', type: :request do
       get new_user_path
     end
 
-    it "リクエストが成功すること" do
+    it 'リクエストが成功すること' do
       expect(response.status).to eq 200
     end
   end
@@ -66,14 +65,14 @@ RSpec.describe '/users', type: :request do
 
     it 'ユーザー名とメールアドレスが表示されていること' do
       expect(response.body).to include 'hoge'
-      expect(response.body).to include "hoge@example.com"
+      expect(response.body).to include 'hoge@example.com'
     end
   end
 
   describe 'POST /create' do
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
-        post users_path, params: { user: FactoryBot.attributes_for(:user) } #パラメータをハッシュとして送信している
+        post users_path, params: { user: FactoryBot.attributes_for(:user) } # パラメータをハッシュとして送信している
         expect(response.status).to eq 302
       end
 
@@ -81,7 +80,7 @@ RSpec.describe '/users', type: :request do
         # expect do
         #   post users_path, params: { user: FactoryBot.attributes_for(:user) }
         # end.to change(User, :count).by(1)
-      expect {post users_path, params: { user: FactoryBot.attributes_for(:user) }}.to change{User.count}.by(1)
+        expect { post users_path, params: { user: FactoryBot.attributes_for(:user) } }.to change { User.count }.by(1)
       end
 
       it 'リダイレクトすること' do
@@ -92,40 +91,34 @@ RSpec.describe '/users', type: :request do
 
     context 'パラメータが不正な場合' do
       it 'リクエストが成功すること' do
-        post users_path, params: { user: FactoryBot.attributes_for(:user, name:"") }
+        post users_path, params: { user: FactoryBot.attributes_for(:user, name: '') }
         expect(response.status).to eq 200
       end
 
-      it 'ユーザーが登録されないこと' do
-        expect do
-          post users_path, params: { user: FactoryBot.attributes_for(:user, name:"") }
-        end.to_not change(User, :count)
-      end
-
       it 'エラーが表示されること' do
-        post users_path, params: { user: FactoryBot.attributes_for(:user, name:"") }
+        post users_path, params: { user: FactoryBot.attributes_for(:user, name: '') }
         expect(response.body).to include 'prohibited this user from being saved'
       end
     end
   end
-      
+
   describe 'PATCH /update' do
     context 'パラメータが妥当な場合' do
       it 'リクエストが成功すること' do
         user = FactoryBot.create :user
-        patch user_path(user.id), params: {user: FactoryBot.attributes_for(:user, name:"ユーザーA")}
+        patch user_path(user.id), params: { user: FactoryBot.attributes_for(:user, name: 'ユーザーA') }
         expect(response.status).to eq 302
       end
 
       it 'ユーザー名が更新されること' do
         user = FactoryBot.create :user
-        expect {patch user_path(user.id), params: {user: FactoryBot.attributes_for(:user, name:"ユーザーA")}}.
-        to change {User.find(user.id).name}.from('hoge').to('ユーザーA')
+        expect { patch user_path(user.id), params: { user: FactoryBot.attributes_for(:user, name: 'ユーザーA') } }
+          .to change { User.find(user.id).name }.from('hoge').to('ユーザーA')
       end
 
       it 'リダイレクトすること' do
         user = FactoryBot.create :user
-        patch user_path(user.id), params: {user: FactoryBot.attributes_for(:user, name:"ユーザーA")}
+        patch user_path(user.id), params: { user: FactoryBot.attributes_for(:user, name: 'ユーザーA') }
         expect(response).to redirect_to User.last
       end
     end
@@ -133,19 +126,19 @@ RSpec.describe '/users', type: :request do
     context 'パラメータが不正な場合' do
       it 'リクエストが成功すること' do
         user = FactoryBot.create :user
-        patch user_path(user.id), params: {user: FactoryBot.attributes_for(:user, name:"")}
+        patch user_path(user.id), params: { user: FactoryBot.attributes_for(:user, name: '') }
         expect(response.status).to eq 200
       end
 
       it 'ユーザー名が変更されないこと' do
         user = FactoryBot.create :user
-        expect {patch user_path(user.id), params: {user: FactoryBot.attributes_for(:user, name:"")}}.
-        not_to change {User.find(user.id).name}
+        expect { patch user_path(user.id), params: { user: FactoryBot.attributes_for(:user, name: '') } }
+          .to_not(change { user.name })
       end
 
       it 'エラーが表示されること' do
         user = FactoryBot.create :user
-        patch user_path(user.id), params: {user: FactoryBot.attributes_for(:user, name:"")}
+        patch user_path(user.id), params: { user: FactoryBot.attributes_for(:user, name: '') }
         expect(response.body).to include 'prohibited this user from being saved'
       end
     end
